50530a109868e6657e6858b231d4868b7f40d1f5
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-07-29 18:44:30 +0530

    Added profile picture of bot

--- main.js
+++ main.js
@@ -1,52 +1,61 @@
-const fs = require('fs');
-const Discord = require('discord.js');
+const fs = require("fs");
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const { prefix, token } = require('./configs/config.json');
+const { prefix, token } = require("./configs/config.json");
 
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
-    console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
+  client.user
+    .setAvatar("./images/logo.png")
+    .then((user) => console.log(`New avatar set!`))
+    .catch(console.error);
 });
 
 // Create an event listener for new guild members
-client.on('guildMemberAdd', member => {
-    // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
-    // Do nothing if the channel wasn't found on this server
-    if (!channel) return;
-    // Send the message, mentioning the member
-    channel.send(`Welcome to the server, ${member}`);
-  });
+client.on("guildMemberAdd", (member) => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(
+    (ch) => ch.name === "welcome"
+  );
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
 
-client.on('message', message => {
-    console.log(`Message string:${message.toString()}\nSent by:${message.author}`);
+client.on("message", (message) => {
+  console.log(
+    `Message string:${message.toString()}\nSent by:${message.author}`
+  );
 
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    if (message.channel.type === 'dm' || message.channel.name !== 'testing'){
-        message.reply('Hello there');
-        return;
-    }
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
+  if (message.channel.type === "dm" || message.channel.name !== "testing") {
+    message.reply("Hello there");
+    return;
+  }
 
-    if (!client.commands.has(command)) return;
+  if (!client.commands.has(command)) return;
 
-    try {
-        client.commands.get(command).execute(message, args);
-    } catch (error) {
-        console.error(error);
-        message.reply('there was an error trying to execute that command!');
-    }
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
+  }
 });
 
 client.login(token);
